95c3380f38f5e52ee565a06dd41bdcd409090af6
Valeriia <valeria.chyornaya@gmail.com>
2020-02-18 23:41:55 +0200

    Updates

---
+++ precache-manifest.ad688313ab0967cb3812c474ec944dac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "573ae9b8113ea2f684f13ae86578e15e",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "d2fad385875d58c98ef6",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.9855299a.chunk.css"
+  },
+  {
+    "revision": "05c5bbc00d8b7b1cab0c",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.42d8cca9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.42d8cca9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2fad385875d58c98ef6",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.d6f42451.chunk.js"
+  },
+  {
+    "revision": "8b583280b34de9d01efa",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.8935b325.js"
+  }
+]);
